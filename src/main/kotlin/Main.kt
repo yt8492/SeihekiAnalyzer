c66a048f37e0dacab26394830c0bcfd2fb1e27c1
yt8492 <tmymyut2000@gmail.com>
2019-02-24 15:14:42 +0900

    クラスに分けた

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,151 +1,51 @@
-import org.jsoup.Connection
-import org.jsoup.Jsoup
-import java.net.SocketTimeoutException
-import java.net.UnknownHostException
-
 fun main() {
-    val loginUrl = "https://login.dlsite.com/login"
-    val mainUrl = "https://ssl.dlsite.com/maniax/mypage/userbuy"
-
     val console = System.console() ?: null
 
     print("UserId: ")
-    val userId = console?.readLine() ?: readLine() ?: return
+    val userId = console?.readLine() ?: readLine() ?: ""
     print("Password: ")
-    val password = console?.readPassword()?.joinToString("") ?: readLine() ?: return
+    val password = console?.readPassword()?.joinToString("") ?: readLine() ?: ""
 
-    val res1 = getConnection(loginUrl)
-        .execute()
-    val welcomePage = res1.parse()
-    val welcomeCookie = res1.cookies()
-    val token = welcomePage.select("[name=_token]").first()
-    val tokenKey = token.attr("name")
-    val tokenValue = token.attr("value")
-
-    val cookies = requestByPost(loginUrl, cookies = welcomeCookie, data = mapOf(
-        tokenKey to tokenValue,
-        "login_id" to userId,
-        "password" to password
-    )).cookies()
-
-    val historyCookies = requestByPost(mainUrl, cookies = cookies).cookies()
-
-    if (cookies.containsKey("PHPSESSID")) {
-        println("ログインに成功しました。")
-    } else {
-        println("ログインに失敗しました。")
-        return
-    }
-
-    println("購入履歴取得中...")
-
-    val thisMonthHistory = requestByGet(mainUrl, cookies = historyCookies).parse()
-
-    val pastMonthHistory = requestByGet(mainUrl, cookies = historyCookies, data = mapOf(
-        "_layout" to "mypage_userbuy_complete",
-        "_form_id" to "mypageUserbuyCompleteForm",
-        "_site" to "maniax",
-        "_view" to "input",
-        "start" to "all"
-    )).parse()
-
-    val urls1 = thisMonthHistory
-        .getElementsByClass("work_name")
-        .map {
-            it.select("[href]").toString().split("\"")[1]
+    SeihekiAnalyzer.loginAndAnalyze(userId, password, object : SeihekiAnalyzer.Callback {
+        override fun onLoginSuccess() {
+            println("ログインに成功しました。")
         }
 
-    val urls2 = pastMonthHistory
-        .getElementsByClass("work_name")
-        .map {
-            it.select("[href]").toString().split("\"")[1]
+        override fun onLoginFailure() {
+            println("ログインに失敗しました。")
         }
 
-    val urls = urls1 + urls2
-
-    println(
-        urls.mapIndexed { index, url ->
-            "${"%3d".format(index + 1)}: $url"
-        }.joinToString("\n")
-    )
-
-    val genreCnt = mutableMapOf<String, Int>()
-    var totalCnt = 0
-
-    urls.forEach { url ->
-        if (analyzeTag(genreCnt, url)) {
-            totalCnt++
+        override fun onStartUrlFetch() {
+            println("購入履歴取得中...")
         }
-    }
 
-    println("\n作品数: ${urls.size}, 失敗: ${urls.size - totalCnt}\n")
-    println(
-        genreCnt.map { it.toPair() }
-            .sortedByDescending { it.second }
-            .joinToString("\n") {
-                "%s: %.2f %%".format(it.first, (it.second.toDouble() / totalCnt) * 100)
-            }
-    )
-}
-
-fun analyzeTag(genreCnt: MutableMap<String, Int>, url: String, failurCnt: Int = 0): Boolean {
-    try {
-        when {
-            failurCnt == 0 -> println("Analyzing: $url")
-            failurCnt < 5 -> println("Retrying: $url")
-            else -> {
-                println("Error: $url")
-                return false
-            }
+        override fun onUrlsFetchSuccess(urls: List<String>) {
+            println(
+                urls.mapIndexed { index, url ->
+                    "${"%3d".format(index + 1)}: $url"
+                }.joinToString("\n")
+            )
         }
-        val voicePage = requestByGet(url).parse()
-        val rows = voicePage.getElementById("work_outline").select("tr")
-        val tags = rows.find { row ->
-            row.child(0).text() == "ジャンル"
-        }?.getElementsByClass("main_genre")?.select("[href]")?.text()?.split(" ".toRegex()) ?: listOf()
-        tags.forEach { tag ->
-            var cnt = genreCnt[tag]
-            cnt = if (cnt == null) {
-                1
+
+        override fun onAnalyzingTag(url: String, retryFlg: Boolean) {
+            if (retryFlg) {
+                println("Analyzing: $url")
             } else {
-                cnt + 1
+                println("Retrying: $url")
             }
-            genreCnt[tag] = cnt
         }
-        return true
-    } catch (e: SocketTimeoutException) {
-        return analyzeTag(genreCnt, url, failurCnt + 1)
-    } catch (e: UnknownHostException) {
-        return analyzeTag(genreCnt, url, failurCnt + 1)
-    } catch (e: Exception) {
-        e.printStackTrace()
-        return analyzeTag(genreCnt, url, failurCnt + 1)
-    }
-}
 
-fun requestByGet(url: String, cookies: Map<String, String>? = null, data: Map<String, String>? = null): Connection.Response {
-    var connection = getConnection(url)
-    if (cookies != null) {
-        connection = connection.cookies(cookies)
-    }
-    if (data != null) {
-        connection = connection.data(data)
-    }
-    return connection.method(Connection.Method.GET).execute()
-}
+        override fun onTagAnalyzeFailure(url: String) {
+            println("Error: $url")
+        }
 
-fun requestByPost(url: String, cookies: Map<String, String>? = null, data: Map<String, String>? = null): Connection.Response {
-    var connection = getConnection(url)
-    if (cookies != null) {
-        connection = connection.cookies(cookies)
-    }
-    if (data != null) {
-        connection = connection.data(data)
-    }
-    return connection.method(Connection.Method.POST).execute()
-}
-
-fun getConnection(url: String): Connection {
-    Thread.sleep(1000)
-    return Jsoup.connect(url).timeout(100000)
+        override fun onFinishTagsAnalyze(tags: List<SeihekiAnalyzer.Tag>, urls: List<String>, totalCnt: Int) {
+            println("\n作品数: ${urls.size}, 失敗: ${urls.size - totalCnt}\n")
+            println(
+                tags.sortedByDescending { it.count }.joinToString(separator = "\n") {
+                    "%s: %.2f %%".format(it.name, (it.count.toDouble() / totalCnt) * 100)
+                }
+            )
+        }
+    })
 }
